b02f849ff12b4b3c3bd2078bc26159d7b1a81aca
Sean Sutherland <sssuthe@gmail.com>
2019-07-04 11:25:32 -0600

    updating ui configs with dns based url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,14 +4,14 @@ export const environment = {
   oidc: {
     clientId: '0oalqrcqbptWL1ZLA356',
     issuer: 'https://dev-686145.okta.com/oauth2/default',
-    redirectUri: 'https://104.198.141.243/implicit/callback',
+    redirectUri: 'https://sssuthe.com/implicit/callback',
     scope: 'openid profile email',
     testing: {
       disableHttpsCheck: false
     }
   },
   resourceServer: {
-    messagesUrl: 'https://104.198.141.243/api/messages',
-    accountsUrl: 'https://104.198.141.243/accounts'
+    messagesUrl: 'https://sssuthe.com/api/messages',
+    accountsUrl: 'https://sssuthe.com/accounts'
   }
 };
