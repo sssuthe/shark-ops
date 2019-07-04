72d80c7ccb618729d98769ded14c8edf069aabcb
Sean Sutherland <sssuthe@gmail.com>
2019-07-04 09:00:29 -0600

    updating prod config with new url for prod cluster

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,14 +4,14 @@ export const environment = {
   oidc: {
     clientId: '0oalqrcqbptWL1ZLA356',
     issuer: 'https://dev-686145.okta.com/oauth2/default',
-    redirectUri: 'https://192.168.99.100/implicit/callback',
+    redirectUri: 'http://104.198.141.243/implicit/callback',
     scope: 'openid profile email',
     testing: {
       disableHttpsCheck: false
     }
   },
   resourceServer: {
-    messagesUrl: 'https://192.168.99.100/api/messages',
-    accountsUrl: 'https://192.168.99.100/accounts'
+    messagesUrl: 'http://104.198.141.243/api/messages',
+    accountsUrl: 'http://104.198.141.243/accounts'
   }
 };
