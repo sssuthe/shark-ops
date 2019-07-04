a1a822d6e4bea0a06846a2772530353608b6d9ff
Sean Sutherland <sssuthe@gmail.com>
2019-07-04 11:13:39 -0600

    updating ui configs with new https urls

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,14 +4,14 @@ export const environment = {
   oidc: {
     clientId: '0oalqrcqbptWL1ZLA356',
     issuer: 'https://dev-686145.okta.com/oauth2/default',
-    redirectUri: 'http://104.198.141.243/implicit/callback',
+    redirectUri: 'https://104.198.141.243/implicit/callback',
     scope: 'openid profile email',
     testing: {
       disableHttpsCheck: false
     }
   },
   resourceServer: {
-    messagesUrl: 'http://104.198.141.243/api/messages',
-    accountsUrl: 'http://104.198.141.243/accounts'
+    messagesUrl: 'https://104.198.141.243/api/messages',
+    accountsUrl: 'https://104.198.141.243/accounts'
   }
 };
